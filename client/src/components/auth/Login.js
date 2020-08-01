4cdfd40f2d0d380a8cc3c43c7f8b02c344e2e8e4
unknown <rajeshsachin786@gmail.com>
2020-08-01 17:19:55 +0530

    adding auth in client side

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect  } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
+    const [ formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const {  email, password } = formData;
+    
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+     
+    const onSubmit  = e => {
+        e.preventDefault();
+        login(email, password);
+    }
+
+    // Redirect if logged in
+    if(isAuthenticated){
+      return <Redirect to = "/dashboard" />
+    }
     return (
-        <div>
-           Login 
+       <Fragment>
+        <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit = { onSubmit } >
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value = {email} onChange = { onChange} />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
         </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value = {password}
+            onChange = { onChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login"  />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to ="/register">Sign Up</Link>
+      </p>
+       </Fragment> 
     )
 }
 
-export default Login
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { login })(Login)
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,12 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [ formData, setFormData] = useState({
         name: '',
         email: '',
@@ -14,11 +20,19 @@ const Register = () => {
     const onSubmit  = e => {
         e.preventDefault();
         if(password !== password2){
-            console.log('Passwords do not match')
+            setAlert('Password not match', 'danger')
         } else {
-            console.log(formData)
+           register({
+             name,
+             email, 
+             password
+           });
         }
     }
+
+if(isAuthenticated){
+  return <Redirect to = "/dashboard" />
+}
     return (
        <Fragment>
         <h1 className="large text-primary">Sign Up</h1>
@@ -57,10 +71,22 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register"  />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to ="/login">Sign In</Link>
       </p>
        </Fragment> 
     )
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+
+
+export default connect(mapStateToProps, { setAlert, register })(Register)
