91be1e9fddd48bbc9b6953ff47242906239815f1
Zluck Solutions <rajeshsachin786@gmail.com>
2020-08-24 00:06:37 -0700

    Adding dashboard in client side

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
-
 
 const app = express();
 
 //connect database
 
-connectDB()
+connectDB();
 
 //Define body-parser
 app.use(express.json());
+app.use(cors());
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/posts', require('./routes/api/posts'));
 
-app.get('/', (req, res) => res.send('testing'))
+app.get('/', (req, res) => res.send('testing'));
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
