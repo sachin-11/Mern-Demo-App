5e1bd4dbbadc143f8215cb113d3737ec2809121a
unknown <rajeshsachin786@gmail.com>
2020-10-12 23:37:41 +0530

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,27 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Dashboard from './components/Dashboard/Dashboard';
-import Alert from './components/layout/Alert';
-import PrivateRoute from './components/routing/PrivateRoute';
-
-//Redux
-
-import { Provider } from 'react-redux';
-import store from './store';
-import setAuthToken from './utils/setAuthToken';
-import { loadUser } from './actions/auth';
-
-import './App.css';
-
+import Landing from './components/layout/Landing'
+import './App.css'
 
 const App = () => {
-  useEffect(() => {
-    setAuthToken(localStorage.token);
-    store.dispatch(loadUser());
-  }, []);
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <div className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/Login' component={Login} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
-};
+    <Router>
+       <Fragment>
+      <Navbar />
+      <Route exact path= '/' component={Landing} />
+      <section className = "container">
+         <Switch>
+           <Route exact path='/register' component = {Register} />
+           <Route exact path= '/login' component = {Login} />
+         </Switch>
+      </section>
+    </Fragment>
+    </Router>
+ 
+  )
+}
 
-export default App;
+export default App
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,55 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { logout } from '../../actions/auth';
 
-
-
-const Navbar = ({ auth: { isAuthenticated, loading}, logout}) => {
-
-  const authLinks = (
-    <ul>
-    <li>
-      <a onClick = {logout}  href='#!'>
-        <i className='fas fa--sign-out-alt'></i>{ ''}
-        <span className = "hide-sm">Logout</span></a>
-    </li>
-  </ul>
-  )
-
-  const guestLinks = (
-    <ul>
-    <li>
-      <a href='#!'>Developers</a>
-    </li>
-    <li>
-      <Link to='/register'>Register</Link>
-    </li>
-    <li>
-      <Link to='/login'>Login</Link>
-    </li>
-  </ul>
-  )
-  return (
-    <nav className='navbar bg-dark'>
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
       <h1>
-        <a href='index.html'>
-          <i className='fas fa-code'></i> DevConnector
-        </a>
+        <Link to="/index"><i className="fas fa-code"></i> DevConnector</Link>
       </h1>
-  {!loading && ( <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
+      <ul>
+        <li><Link to="/profiles">Developers</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
     </nav>
-  );
-};
-
-Navbar.propTypes = {
-  logout: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+    )
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth
-})
-
-export default connect(mapStateToProps, { logout})(Navbar);
+export default Navbar
